dadacaf555c18be6c3bb407a35305512e18fe819
sinethemba <poni.sneh@gmail.com>
2020-03-18 14:31:42 +0200

    commit..

--- src/test/java/DataStructuresTest.java
+++ src/test/java/DataStructuresTest.java
@@ -17,31 +17,57 @@ public class DataStructuresTest {
     @Test
     public void map() {
 
-        DataStructures map = new DataStructures("keyElement1", "Sinethemba");
-        DataStructures map2 = new DataStructures("keyElement2","Poni");
-
+        DataStructures map = new DataStructures("key1","Sinethemba");
+        DataStructures map2 = new DataStructures("key2","Poni");
 
         n.map(map);
         n.map(map2);
 
 
-        n.retrieveMap("key element1");
-        n.retrieveMap("Key element2");
 
-        n.deleteMap("Key element2");
+        n.retrieveMap("key1");
+        n.retrieveMap("key2");
 
+        n.deleteMap("key2");
 
     }
 
     @Test
     public void set() {
+
+        n.set("Sinethemba");
+        n.set("Poni");
+        n.set("5");
+
+        n.retrieveSet();
+
     }
 
     @Test
     public void deleteSet() {
+
+
+        n.set("Sinethemba");
+        n.set("Poni");
+        n.set("4");
+
+
+        n.deleteSet();
+
+
     }
 
     @Test
     public void list() {
+
+        n.list(6);
+        n.list(33);
+        n.list(38);
+
+        n.retrieveList();
+
+        n.deleteList();
+        n.retrieveList();
+
     }
 }
